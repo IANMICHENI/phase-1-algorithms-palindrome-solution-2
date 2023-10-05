d8df2dbacfacee35988cd41760c8619f0bcbd899
ian <ianmwendamicheni@gmail.com>
2023-10-05 12:07:57 +0300

    done assignment

--- index.js
+++ index.js
@@ -1,18 +1,14 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  for (let startIndex = 0; startIndex < Math.floor(word.length / 2); startIndex++) {
+    const endIndex = word.length - 1 - startIndex;
+    
+    if (word[startIndex] !== word[endIndex]) {
+      return false;
+    }
+  }
+  return true;
 }
-
-/* 
-  Add your pseudocode here
-*/
-
-/*
-  Add written explanation of your solution here
-*/
-
-// You can run `node index.js` to view these console logs
 if (require.main === module) {
-  // add your own custom tests in here
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
 
@@ -23,3 +19,4 @@ if (require.main === module) {
 }
 
 module.exports = isPalindrome;
+
